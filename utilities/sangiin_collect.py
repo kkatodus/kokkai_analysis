560a05e67f3d0dab0a0893f6d0ae6cd374db23e6
katok <kkatodus@gmail.com>
2022-06-21 15:53:51 -0400

    now able to collect the party votes

--- utilities/sangiin_collect.py
+++ utilities/sangiin_collect.py
@@ -3,8 +3,8 @@ import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import time
 import os
+import re
 output_dir = "..\\data_sangiin\\"
 chromedriver_path = "..\\chromedriver.exe"
 main_website_url = "https://www.sangiin.go.jp/japanese/touhyoulist/touhyoulist.html"
@@ -15,6 +15,7 @@ meeting_period_xpath = "//font[contains(text(), '年')]"
 topic_xpath = "//tr//td//p//tt//font//a"
 whole_result_xpath = "//font//b"
 party_vote_xpath = "//caption[@class='party']/following-sibling::tbody"
+party_names_xpath = "//caption[@class='party']"
 whole_result_num_xpath = "//*[contains(text(), '投票総数')]"
 
 meeting_layout = {
@@ -30,6 +31,13 @@ topic_layout = {
             "voting_results":{}
             }
 
+party_vote_results_layout = {
+    "member_num":None, 
+    "yay":None,
+    "nay":None,
+    "votes":[]
+}
+
 def create_meeting_dict_or_load_json(filename):
     meeting_json_path = os.path.join(output_dir, filename)
     if os.path.exists(meeting_json_path):
@@ -42,23 +50,54 @@ def save_json(dict_obj, path):
     with open(path, "w", encoding="utf-8") as j:
         json.dump(dict_obj, j, ensure_ascii=False, indent=4)
 
-def collect_individual_votes(topic_dict):
+def collect_whole_party_vote(party_caption, party_dict):
+    party_vote_numbers = party_caption.text.split("\n")[2]
+    party_vote_numbers = party_vote_numbers.split("　　　")
+    
+    keys = ["yay", "nay"]
+    #getting results of whole party vote
+    for vote, key in zip(party_vote_numbers, keys):
+        vote_num = vote.split(" ")
+        vote_num = [num for num in vote_num if num != ""]
+        vote_num = vote_num[1]
+        party_dict[key] = int(vote_num)
+    #extracting party name and member number
+    party_name_and_mem_num = party_caption.text.split("\n")[0]
+    repl_chars = "(名)"
+    for repl_char in repl_chars:
+        party_name_and_mem_num = party_name_and_mem_num.replace(repl_char, ",")
+    party_name_and_mem_num = party_name_and_mem_num.split(",")
+    party_name = party_name_and_mem_num[0]
+    party_mem_num = int(party_name_and_mem_num[1])
+    party_dict["member_num"] = party_mem_num
+    return party_dict, party_name
+
+def collect_individual_votes(one_topic_dict):
     whole_result = driver.find_element(By.XPATH, whole_result_num_xpath)
     whole_result = whole_result.text.split("　　　")
     whole_result_dict = {}
+
+    #getting the entire voting result of the house
     keys = ["total_votes", "yay", "nay"]
     for key, result in zip(keys, whole_result):
         result = int(result.split("　")[1])
         whole_result_dict[key] = result
+    party_names = driver.find_elements(By.XPATH, party_names_xpath)
+    party_votes = driver.find_elements(By.XPATH, party_vote_xpath)
 
-    print(whole_result)
-    topic_dict["whole_result"] = whole_result_dict
-    return topic_dict
+    #collecting individual voting results for each party
+    for party_name, party_vote in zip(party_names, party_votes):
+        party_dict = party_vote_results_layout.copy()
+        party_dict, party_name = collect_whole_party_vote(party_name, party_dict)
+        one_topic_dict["voting_results"][party_name] = party_dict
+
+    one_topic_dict["whole_result"] = whole_result_dict
+    return one_topic_dict
 
 def collect_whole_result(topic_dict):
     whole_result = driver.find_element(By.XPATH, whole_result_xpath).text
     topic_dict["whole_result"] = whole_result
-
+    topic_dict["voting_results"] = None
     return topic_dict
 
 def check_for_individual_votes():
@@ -116,7 +155,8 @@ def iterate_meetings():
         meeting_file_name = f"{meeting_name}.json"
         meeting_path = os.path.join(output_dir, meeting_file_name)
         print("Saving Json")
-        print(meeting_dict)
         save_json(meeting_dict, meeting_path)
+
+    
 if __name__ == "__main__":
     iterate_meetings()
--- utilities/test.py
+++ utilities/test.py
@@ -1,15 +1,12 @@
 import json
 import os
+import re
 
-year = 2020
-data_dir_path = f"C:\\Users\\katok\\Documents\\Projects\\kokkai_analysis\\data\\{year}\\"
 
-file_list = sorted(os.listdir(data_dir_path), key=lambda x:x.split("_")[0])
-first_file_name = file_list[0]
-print("First_file", first_file_name)
-first_file_path = f"{data_dir_path}{first_file_name}"
-with open(first_file_path, "r", encoding = "utf-8") as f:
-    first_file_json = json.load(f)
-    meeting_content_summary = first_file_json["meetingRecord"][0]["speechRecord"][0]["speech"].split("\r\n")
-    meeting_content_summary = [content.replace("\u3000","") for content in meeting_content_summary]
-    print(meeting_content_summary)
+a = "自由民主党・国民の声(110名)"
+
+repl_chars = "(名)"
+for repl_char in repl_chars:
+    a = a.replace(repl_char, ",")
+
+print(a.split(","))
