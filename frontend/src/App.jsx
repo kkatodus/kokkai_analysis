2118c84855b32b18165033f841c034aa511ddd93
katok <kkatodus@gmail.com>
2023-07-10 21:31:34 -0400

    build dir

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import React from 'react';
 import LandingPage from './pages/LandingPage';
-// import SangiinMeetingsPage from './pages/SangiinMeetingsPage';
-// import SangiinMeetingDetailPage from './pages/MeetingDetailPage';
-// import SangiinTopicDetailPage from './pages/SangiinTopicDetailPage';
+import SangiinMeetingsPage from './pages/SangiinMeetingsPage';
+import SangiinMeetingDetailPage from './pages/MeetingDetailPage';
+import SangiinTopicDetailPage from './pages/SangiinTopicDetailPage';
 import InfoPage from './pages/InfoPage';
-// import SangiinMenuPage from './pages/SangiinMenuPage';
-// import SangiinReprPage from './pages/SangiinReprPage';
-// import SangiinCommitteePage from './pages/SangiinCommitteePage';
-// import ShugiinMenuPage from './pages/ShugiinMenuPage';
-// import ShugiinCommitteePage from './pages/ShugiinCommitteePage';
-// import ShugiinReprPage from './pages/ShugiinReprPage';
+import SangiinMenuPage from './pages/SangiinMenuPage';
+import SangiinReprPage from './pages/SangiinReprPage';
+import SangiinCommitteePage from './pages/SangiinCommitteePage';
+import ShugiinMenuPage from './pages/ShugiinMenuPage';
+import ShugiinCommitteePage from './pages/ShugiinCommitteePage';
+import ShugiinReprPage from './pages/ShugiinReprPage';
 /**
  *
  * @return {JSX.Element}
@@ -22,7 +22,7 @@ function App() {
       <Route exact path="/" element={<LandingPage />} />
       <Route exact path="/page_info" element={<InfoPage />} />
       <Route exact path="sangiin_menu" element={<SangiinMenuPage />} />
-      {/* <Route exact path="sangiin_meetings" element={<SangiinMeetingsPage />} />
+      <Route exact path="sangiin_meetings" element={<SangiinMeetingsPage />} />
       <Route exact path="sangiin_repr" element={<SangiinReprPage />} />
       <Route
         exact
@@ -41,7 +41,7 @@ function App() {
       />
       <Route exact path="shugiin_menu" element={<ShugiinMenuPage />} />
       <Route exact path="shugiin_repr" element={<ShugiinReprPage />} />
-      <Route exact path="shugiin_commitee" element={<ShugiinCommitteePage />} /> */}
+      <Route exact path="shugiin_commitee" element={<ShugiinCommitteePage />} />
     </Routes>
   );
 }
