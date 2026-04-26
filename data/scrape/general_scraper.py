20038ae6a88a6d3c42b967ce3ae372271e2bcb0c
Ken Kato <ken.kato@mail.utoronto.ca>
2026-04-26 17:24:50 +0900

    party update

--- data/scrape/general_scraper.py
+++ data/scrape/general_scraper.py
@@ -1,17 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from webdriver_manager.firefox import GeckoDriverManager
 
-firefox_binary_path = "/mnt/c/Program Files/Mozilla Firefox/firefox.exe"
+# firefox_binary_path = "/mnt/c/Program Files/Mozilla Firefox/firefox.exe"/
 
 class GeneralScraper:
     def __init__(self, firefox=False):
         if firefox:
-            options = Options()
-            options.binary_location = firefox_binary_path
-
-            driver = webdriver.Firefox(options=options, executable_path='./geckodriver')
+            options = FirefoxOptions()
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.set_preference("remote.active-protocols", 3)
+            service = FirefoxService(GeckoDriverManager().install())
+            driver = webdriver.Firefox(service=service, options=options)
         else:
             ChromeOptions = webdriver.ChromeOptions()
             ChromeOptions.add_argument('--disable-browser-side-navigation')
